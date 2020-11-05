0a8b073c6b5e37ac88743c722294855d660a0bc7
alizarizqi <54764338+alizarizqi@users.noreply.github.com>
2020-11-05 21:41:29 +0700

    update model & viewmodel

--- app/src/main/java/id/putraprima/mvvmlogin/models/ModelLogin.java
+++ app/src/main/java/id/putraprima/mvvmlogin/models/ModelLogin.java
@@ -1,6 +1,9 @@
 package id.putraprima.mvvmlogin.models;
 
-public class ModelLogin {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ModelLogin implements Parcelable {
     String password;
     String username;
 
@@ -27,4 +30,32 @@ public class ModelLogin {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.password);
+        dest.writeString(this.username);
+    }
+
+    protected ModelLogin(Parcel in) {
+        this.password = in.readString();
+        this.username = in.readString();
+    }
+
+    public static final Creator<ModelLogin> CREATOR = new Creator<ModelLogin>() {
+        @Override
+        public ModelLogin createFromParcel(Parcel source) {
+            return new ModelLogin(source);
+        }
+
+        @Override
+        public ModelLogin[] newArray(int size) {
+            return new ModelLogin[size];
+        }
+    };
 }
--- app/src/main/java/id/putraprima/mvvmlogin/viewmodels/ViewModelLogin.java
+++ app/src/main/java/id/putraprima/mvvmlogin/viewmodels/ViewModelLogin.java
@@ -1,4 +1,6 @@
 package id.putraprima.mvvmlogin.viewmodels;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -8,16 +10,16 @@ public class ViewModelLogin extends ViewModel{
     private MutableLiveData<ModelLogin> loginMutableLiveData = new MutableLiveData<>();
     private ModelLogin modelLogin;
 
+    public ViewModelLogin(ModelLogin modelLogin){
+        this.loginMutableLiveData.setValue(modelLogin);
+
+    }
+
     public void Login(){
-        this.cekUsername();
-        this.cekPassword();
+        Log.d("username", this.loginMutableLiveData.getValue().getUsername());
+        Log.d("password", this.loginMutableLiveData.getValue().getPassword());
 
     }
 
-    public void cekUsername(){
-
-    }
-    public void cekPassword(){
-
-    }
+    
 }
